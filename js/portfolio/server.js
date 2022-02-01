177663a3638909c531bbb5628a2a7ec1176870aa
Reycon95 <62074185+Reycon95@users.noreply.github.com>
2022-02-01 22:37:58 +0100

    Ultima version
    Subida la ultima version

--- js/portfolio/server.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
-console.log("row.type");
-db.each("SELECT DISTINCT type from Items", function(err, row) {
-    console.log(row.type);
-});
-
-db.close();
